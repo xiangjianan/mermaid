3388ffb35e45adee6c7ce2908ae419bd45753f00
xiangjianan <xiang9872@126.com>
2026-07-01 20:51:23 +0800

    ci: deploy app to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH ?? "/",
   plugins: [react()],
   test: {
     exclude: [...configDefaults.exclude, "tests/**/*.spec.ts"],
